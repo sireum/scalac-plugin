2589d205e8afad068fe2d6a0564cfa7e36dc4b40
Robby <robby@k-state.edu>
2017-04-23 22:55:58 -0500

    Updated.

--- src/main/scala/org/sireum/scalac/SireumPlugin.scala
+++ src/main/scala/org/sireum/scalac/SireumPlugin.scala
@@ -45,61 +45,99 @@ final class SireumComponent(val global: Global) extends PluginComponent with Typ
   override val runsRightAfter = Some("parser")
   override val runsAfter: List[String] = runsRightAfter.toList
   override val runsBefore: List[String] = List[String]("namer")
-  def isDollar(t: Tree): Boolean = t match {
+
+  def isDollar(t: Any): Boolean = t match {
     case q"$$" => true
     case _ => false
   }
+
+  def isUninit(t: Any): Boolean = t match {
+    case q"??" => true
+    case _ => false
+  }
+
+
   val unitCons = Literal(Constant(()))
 
   final class Transformer(unit: CompilationUnit,
-                          var methodName: Option[TermName],
-                          var declaredVars: List[String]) extends TypingTransformer(unit) {
+                          inTrait: Boolean) extends global.Transformer {
+    def sup(tree: global.Tree): global.Tree = super.transform(tree)
+
+    def trans(tree: Any): global.Tree = transform(tree.asInstanceOf[global.Tree])
+
     override def transform(tree: global.Tree): global.Tree = tree match {
-      case tree: DefDef =>
-        val mn = methodName
-        val dvs = declaredVars
-        methodName = Some(tree.name)
-        declaredVars = List()
-        val r = super.transform(tree)
-        declaredVars = dvs
-        methodName = mn
-        r
-      case q"{ ..$stats }" =>
-        val dvs = declaredVars
-        val r = super.transform(tree)
-        val diffVars = declaredVars.take(declaredVars.size - dvs.size)
-        val r3 = if (diffVars.nonEmpty) {
-          //println(s"Here $diffVars")
-          val cs: List[Tree] =
-            (for (v <- declaredVars.take(declaredVars.size - dvs.size))
-              yield q"_cleanup(${Ident(v)})").reverse
-          declaredVars = dvs
-          val r2 = r match {
-            case r: Block =>
-              r.expr match {
-                case _: Assign | _: ValDef => Block(r.stats ++ (r.expr :: cs), unitCons)
-                case _ => Block(r.stats ++ cs, r.expr)
-              }
-          }
-          // println(showCode(r2))
-          r2
-        } else r
-        //println(showCode(r3))
-        r3
+      case Apply(Select(Apply(Ident(TermName("StringContext")), _), TermName("s")), _) => tree
+      case q"$_ trait $_[..$_] extends { ..$_ } with ..$_ { $_ => ..$_ }" =>
+        new Transformer(unit, inTrait = true).sup(tree)
+      case tree: DefDef if inTrait =>
+        tree.rhs match {
+          case Apply(Select(Apply(Ident(TermName("StringContext")), List(Literal(Constant(_: String)))), TermName("c")), List()) =>
+            tree.copy(rhs = EmptyTree)
+          case _ => tree
+        }
+      case Literal(Constant(n: Int)) =>
+        q"StringContext(${BigInt(n).toString}).z()"
+      case Literal(Constant(n: Long)) =>
+        q"StringContext(${BigInt(n).toString}).z()"
+      case tree: CaseDef =>
+        val g = transform(tree.guard)
+        val b = transform(tree.body)
+        if ((g ne tree.guard) || (b ne tree.body)) {
+          tree.copy(guard = transform(tree.guard), body = transform(tree.body))
+        } else tree
+      case tree: Apply if tree.args.nonEmpty =>
+        var changed = false
+        val r = tree.copy(args = for (arg <- tree.args) yield arg match {
+          case arg: Literal => transform(arg)
+          case arg: AssignOrNamedArg =>
+            changed = true
+            arg.copy(rhs = q"_assign(${transform(arg.rhs)})")
+          case _ =>
+            changed = true
+            q"_assign(${transform(arg)})"
+        })
+        if (changed) r else tree
+      case tree: Assign => tree.copy(rhs = q"_assign(${transform(tree.rhs)})")
+      case q"$expr1(..$exprs2) = $expr" =>
+        q"${trans(expr1)}(..${exprs2.map(trans)}) = _assign(${trans(expr)})"
+      case q"(..$exprs)" if exprs.size > 1 => q"(..${exprs.map(e => q"_assign(${trans(e)})")})"
       case q"$mods val ${tname: TermName}: $tpt = $expr" =>
-        if (methodName.nonEmpty)
-          declaredVars = tname.decoded :: declaredVars
-        if (!(expr == EmptyTree || isDollar(expr.asInstanceOf[Tree])))
-          return q"$mods var $tname: $tpt = _assign($expr)"
-        tree
+        if (!(expr == EmptyTree || isDollar(expr)))
+          q"$mods var $tname: $tpt = _assign(${trans(expr)})"
+        else tree
       case q"$mods var ${tname: TermName}: $tpt = $expr" =>
-        if (methodName.nonEmpty)
-          declaredVars = tname.decoded :: declaredVars
-        if (!(expr == EmptyTree || isDollar(expr.asInstanceOf[Tree])))
-          return q"$mods var $tname: $tpt = _assign($expr)"
-        tree
-      case q"$lhs = $rhs" => q"$lhs = _assign($rhs)"
-      case q"$expr1(..$exprs2) = $expr" => q"$expr1(..$exprs2) = _assign($expr)"
+        if (isUninit(expr)) {
+          tpt match {
+            case Ident(TypeName(n)) => n match {
+              case "B" => q"$mods var $tname: $tpt = F"
+              case "Z" => q"""$mods var $tname: $tpt = z"0""""
+              case "Z8" => q"""$mods var $tname: $tpt = z8"0""""
+              case "Z16" => q"""$mods var $tname: $tpt = z16"0""""
+              case "Z32" => q"""$mods var $tname: $tpt = z32"0""""
+              case "Z64" => q"""$mods var $tname: $tpt = z64"0""""
+              case "N" => q"""$mods var $tname: $tpt = n"0""""
+              case "N8" => q"""$mods var $tname: $tpt = n8"0""""
+              case "N16" => q"""$mods var $tname: $tpt = n16"0""""
+              case "N32" => q"""$mods var $tname: $tpt = n32"0""""
+              case "N64" => q"""$mods var $tname: $tpt = n64"0""""
+              case "S8" => q"""$mods var $tname: $tpt = s8"0""""
+              case "S16" => q"""$mods var $tname: $tpt = s16"0""""
+              case "S32" => q"""$mods var $tname: $tpt = s32"0""""
+              case "S64" => q"""$mods var $tname: $tpt = s64"0""""
+              case "U8" => q"""$mods var $tname: $tpt = u8"0""""
+              case "U16" => q"""$mods var $tname: $tpt = u16"0""""
+              case "U32" => q"""$mods var $tname: $tpt = u32"0""""
+              case "U64" => q"""$mods var $tname: $tpt = u64"0""""
+              case "F32" => q"""$mods var $tname: $tpt = f32"0.0""""
+              case "F64" => q"""$mods var $tname: $tpt = f64"0.0""""
+              case "R" => q"""$mods var $tname: $tpt = r"0.0""""
+              case _ => q"$mods var $tname: $tpt = null"
+            }
+            case _ => q"$mods var $tname: $tpt = null"
+          }
+        } else if (!(expr == EmptyTree || isDollar(expr)))
+          q"$mods var $tname: $tpt = _assign(${trans(expr)})"
+        else tree
       case _ => super.transform(tree)
     }
   }
@@ -122,9 +160,9 @@ final class SireumComponent(val global: Global) extends PluginComponent with Typ
           }
         }
         val firstLine = sb.toString
-        isSireum = "//#Sireum" == firstLine
+        isSireum = firstLine.contains("#Sireum")
       }
-      if (isSireum) unit.body = new Transformer(unit, None, List()).transform(unit.body)
+      if (isSireum) unit.body = new Transformer(unit, inTrait = false).transform(unit.body)
     }
   }
 }
