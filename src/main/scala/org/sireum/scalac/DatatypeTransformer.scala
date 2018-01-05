c9f091b74edbda02a12b36d9cb7d5246e6e4dcfb
Robby <robby@k-state.edu>
2018-01-05 10:47:39 -0600

    Cleaned up.

---
+++ src/main/scala/org/sireum/scalac/DatatypeTransformer.scala
@@ -0,0 +1,224 @@
+/*
+ Copyright (c) 2017, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.sireum.scalac
+
+import scala.meta._
+import MetaAnnotationTransformer._
+import scala.collection.mutable.{ArrayBuffer => MSeq}
+
+class DatatypeTransformer(mat: MetaAnnotationTransformer) {
+
+  def transform(name: Vector[String], tree: Tree): Unit = {
+    tree match {
+      case tree: Defn.Trait => transformTrait(name, tree)
+      case tree: Defn.Class => transformClass(name, tree)
+      case _ => mat.error(tree.pos, "Slang @datatype can only be used on a class or a trait.")
+    }
+  }
+
+  def transformTrait(name: Vector[String], tree: Defn.Trait): Unit = {
+    if (tree.templ.early.nonEmpty ||
+      tree.templ.self.decltpe.nonEmpty ||
+      tree.templ.self.name.value != "") {
+      mat.error(tree.pos, "Slang @datatype traits have to be of the form '@datatype trait <id> ... { ... }'.")
+      return
+    }
+    val tname = tree.name
+    val tparams = tree.tparams
+    val tVars = tparams.map { tp => Type.Name(tp.name.value) }
+    val tpe = if (tVars.isEmpty) tname else t"$tname[..$tVars]"
+    val (hasHash, hasEqual, hasString) = hasHashEqualString(tpe, tree.templ.stats)
+    val equals =
+      if (hasEqual) {
+        val eCases =
+          List(if (tparams.isEmpty) p"case o: $tname => isEqual(o)"
+          else p"case (o: $tname[..$tVars] @unchecked) => isEqual(o)",
+            p"case _ => false")
+        List(q"override def equals(o: $scalaAny): $scalaBoolean = { if (this eq o.asInstanceOf[$scalaAnyRef]) true else o match { ..case $eCases } }")
+      } else List()
+    val hash = if (hasHash) List(q"override def hashCode: $scalaInt = { hash.hashCode }") else List()
+    val toString =
+      if (hasString) List(q"override def toString: $javaString = { string }")
+      else List()
+    mat.classMembers.getOrElseUpdate(name, MSeq()) ++= (hash.map(_.syntax) ++ equals.map(_.syntax) ++ toString.map(_.syntax))
+    mat.classSupers.getOrElseUpdate(name, MSeq()) += datatypeSig.syntax
+  }
+
+  def transformClass(name: Vector[String], tree: Defn.Class): Unit = {
+    val q"..$_ class $tname[..$tparams] ..$_ (...$paramss) extends $_" = tree
+    val tVars = tparams.map { tp => Type.Name(tp.name.value) }
+    val tpe = if (tVars.isEmpty) tname else t"$tname[..$tVars]"
+    val (hasHash, hasEqual, hasString) = hasHashEqualString(tpe, tree.templ.stats)
+    if (paramss.nonEmpty && paramss.head.nonEmpty) {
+      var cparams: Vector[String] = Vector()
+      var applyParams: Vector[Term.Param] = Vector()
+      var oApplyParams: Vector[Term.Param] = Vector()
+      var applyArgs: Vector[Term.Name] = Vector()
+      var unapplyTypes: Vector[Type] = Vector()
+      var unapplyArgs: Vector[Term.Name] = Vector()
+      for (param <- paramss.head) {
+        if (param.decltpe.nonEmpty) {
+          val tpeopt = param.decltpe
+          val varName = Term.Name(param.name.value)
+          val hidden = param.mods.exists({
+            case mod"@hidden" => true
+            case _ => false
+          })
+          cparams :+= varName.value
+          applyParams :+= param"$varName: $tpeopt = this.$varName"
+          oApplyParams :+= param"$varName: $tpeopt"
+          applyArgs :+= varName
+          if (!hidden) {
+            val Some(tpe) = tpeopt
+            unapplyTypes :+= tpe
+            unapplyArgs :+= varName
+          }
+        } else {
+          mat.error(param.pos, "Unsupported Slang @datatype parameter form.")
+          return
+        }
+      }
+
+      {
+        val hashCode =
+          if (hasHash) q"override lazy val hashCode: $scalaInt = hash.hashCode"
+          else if (hasEqual) q"override lazy val hashCode: $scalaInt = 0"
+          else q"override lazy val hashCode: $scalaInt = { $scalaSeqQ(this.getClass, ..${unapplyArgs.toList}).hashCode }"
+        val equals =
+          if (hasEqual) {
+            val eCases = Vector(if (tparams.isEmpty) p"case o: $tname => isEqual(o)"
+            else p"case (o: $tname[..$tVars] @unchecked) => isEqual(o)", p"case _ => false")
+            q"override def equals(o: $scalaAny): $scalaBoolean = { if (this eq o.asInstanceOf[$scalaAnyRef]) true else o match { ..case ${eCases.toList} } }"
+          } else {
+            val eCaseEqs = unapplyArgs.map(arg => q"$arg == o.$arg")
+            val eCaseExp = if (eCaseEqs.isEmpty) q"true" else eCaseEqs.tail.foldLeft(eCaseEqs.head)((t1, t2) => q"$t1 && $t2")
+            val eCases =
+              Vector(if (tparams.isEmpty) p"case o: $tname => if (this.hashCode != o.hashCode) false else $eCaseExp"
+              else p"case (o: $tname[..$tVars] @unchecked) => if (this.hashCode != o.hashCode) false else $eCaseExp",
+                p"case _ => false")
+            q"override def equals(o: $scalaAny): $scalaBoolean = { if (this eq o.asInstanceOf[$scalaAnyRef]) true else o match { ..case ${eCases.toList} } }"
+          }
+        val apply = q"def apply(..${applyParams.toList}): $tpe = { new $tname(..${applyArgs.toList}) }"
+        val toString = {
+          var appends = applyArgs.map(arg => q"sb.append($sireumStringEscape($arg))")
+          appends =
+            if (appends.isEmpty) appends
+            else appends.head +: appends.tail.flatMap(a => Vector(q"""sb.append(", ")""", a))
+          if (hasString) Vector(q"override def toString: $javaString = { string }")
+          else Vector(q"""override def toString: $javaString = {
+                            val sb = new _root_.java.lang.StringBuilder
+                            sb.append(${Lit.String(tname.value)})
+                            sb.append('(')
+                            ..${appends.toList}
+                            sb.append(')')
+                            sb.toString
+                          }""",
+            q"override def string: $sireumString = { toString }")
+        }
+        val content = {
+          var fields = List[Term](q"(${Lit.String("type")}, $scalaListQ[$javaString](..${(mat.packageName :+ tname.value).map(x => Lit.String(x)).toList}))")
+          for (x <- applyArgs) {
+            fields ::= q"(${Lit.String(x.value)}, $x)"
+          }
+          q"override lazy val content: $scalaSeq[($javaString, $scalaAny)] = $scalaSeqQ(..${fields.reverse})"
+        }
+        mat.classMembers.getOrElseUpdate(name, MSeq()) ++= toString.map(_.syntax) :+ hashCode.syntax :+ equals.syntax :+ apply.syntax :+ content.syntax
+        mat.classSupers.getOrElseUpdate(name, MSeq()) += datatypeSig.syntax
+        mat.classContructorVals.getOrElseUpdate(name, MSeq()) ++= cparams
+      }
+
+      {
+        val (apply, unapply) =
+          if (tparams.isEmpty)
+            (q"def apply(..${oApplyParams.toList}): $tpe = { new $tname(..${applyArgs.toList}) }",
+              unapplyTypes.size match {
+                case 0 => q"def unapply(o: $tpe): $scalaBoolean = { true }"
+                case 1 => q"def unapply(o: $tpe): $scalaOption[${unapplyTypes.head}] = { $scalaSomeQ(o.${unapplyArgs.head}) }"
+                case n if n <= 22 => q"def unapply(o: $tpe): $scalaOption[(..${unapplyTypes.toList})] = { $scalaSomeQ((..${unapplyArgs.map(arg => q"o.$arg").toList})) }"
+                case _ =>
+                  val unapplyTypess = unapplyTypes.grouped(22).map(types => t"(..${types.toList})").toList
+                  val unapplyArgss = unapplyArgs.grouped(22).map(args => q"(..${args.map(a => q"o.$a").toList})").toList
+                  q"def unapply(o: $tpe): $scalaOption[(..$unapplyTypess)] = { $scalaSomeQ((..$unapplyArgss)) }"
+              })
+          else
+            (q"def apply[..$tparams](..${oApplyParams.toList}): $tpe = { new $tname(..${applyArgs.toList}) }",
+              unapplyTypes.size match {
+                case 0 => q"def unapply[..$tparams](o: $tpe): $scalaBoolean = { true }"
+                case 1 => q"def unapply[..$tparams](o: $tpe): $scalaOption[${unapplyTypes.head}] = { $scalaSomeQ(o.${unapplyArgs.head}) }"
+                case n if n <= 22 => q"def unapply[..$tparams](o: $tpe): $scalaOption[(..${unapplyTypes.toList})] = { $scalaSomeQ((..${unapplyArgs.map(arg => q"o.$arg").toList})) }"
+                case _ =>
+                  val unapplyTypess = unapplyTypes.grouped(22).map(types => t"(..${types.toList})").toList
+                  val unapplyArgss = unapplyArgs.grouped(22).map(args => q"(..${args.map(a => q"o.$a").toList})").toList
+                  q"def unapply[..$tparams](o: $tpe): $scalaOption[(..$unapplyTypess)] = { $scalaSomeQ((..$unapplyArgss)) }"
+              })
+        mat.companionMembers.getOrElseUpdate(name, MSeq()) ++= Vector(apply.syntax, unapply.syntax)
+      }
+    } else {
+      {
+        val hashCode =
+          if (hasHash) q"override val hashCode: $scalaInt = { hash.hashCode }"
+          else if (hasEqual) q"override val hashCode: $scalaInt = { 0 }"
+          else q"override val hashCode: $scalaInt = { this.getClass.hashCode }"
+        val equals =
+          if (hasEqual) {
+            val eCases =
+              Vector(if (tparams.isEmpty) p"case o: $tname => isEqual(o)"
+              else p"case (o: $tname[..$tVars] @unchecked) => isEqual(o)",
+                p"case _ => false")
+            q"override def equals(o: $scalaAny): $scalaBoolean = { if (this eq o.asInstanceOf[$scalaAnyRef]) true else o match { ..case ${eCases.toList} } }"
+          } else {
+            val eCases =
+              Vector(if (tparams.isEmpty) p"case o: $tname => true"
+              else p"case (o: $tname[..$tVars] @unchecked) => true",
+                p"case _ => false")
+            q"override def equals(o: $scalaAny): $scalaBoolean = { if (this eq o.asInstanceOf[$scalaAnyRef]) true else o match { ..case ${eCases.toList} } }"
+          }
+        val toString = {
+          if (hasString) Vector(q"override def toString: $javaString = { string }")
+          else Vector(q"""override def toString: $javaString = { ${Lit.String(tname.value + "()")} }""",
+            q"override def string: $sireumString = { toString }")
+        }
+        val content = q"override lazy val content: $scalaSeq[($javaString, $scalaAny)] = $scalaSeqQ((${Lit.String("type")}, $scalaListQ[$javaString](..${(mat.packageName :+ tname.value).map(x => Lit.String(x)).toList})))"
+        mat.classMembers.getOrElseUpdate(name, MSeq()) ++= toString.map(_.syntax) :+ hashCode.syntax :+ equals.syntax :+ content.syntax
+        mat.classSupers.getOrElseUpdate(name, MSeq()) += datatypeSig.syntax
+      }
+
+      {
+        val (v, apply, unapply) =
+          if (tparams.isEmpty)
+            (q"private[this] val v: $scalaAnyRef = { new $tname() }",
+              q"def apply(): $tpe = { v.asInstanceOf[$tpe] }",
+              q"def unapply(o: $tpe): $scalaBoolean = { true }")
+          else
+            (q"private[this] val v: $scalaAnyRef = { new ${t"$tname[..${tparams.map(_ => scalaNothing)}]"}() }",
+              q"def apply[..$tparams](): $tpe = { v.asInstanceOf[$tpe] }",
+              q"def unapply[..$tparams](o: $tpe): $scalaBoolean = { true }")
+        mat.companionMembers.getOrElseUpdate(name, MSeq()) ++= Vector(v.syntax, apply.syntax, unapply.syntax)
+      }
+    }
+  }
+
+}
---
+++ src/main/scala/org/sireum/scalac/EnumTransformer.scala
@@ -0,0 +1,98 @@
+/*
+ Copyright (c) 2017, Robby, Kansas State University
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.sireum.scalac
+
+import scala.meta._
+import MetaAnnotationTransformer._
+import scala.collection.mutable.{ArrayBuffer => MSeq}
+
+class EnumTransformer(mat: MetaAnnotationTransformer) {
+
+  def transform(name: Vector[String], tree: Tree): Unit = {
+    tree match {
+      case tree: Defn.Object =>
+        if (tree.templ.early.nonEmpty ||
+          tree.templ.inits.nonEmpty ||
+          tree.templ.self.decltpe.nonEmpty ||
+          tree.templ.self.name.value != "") {
+          mat.error(tree.pos, s"Invalid @enum form on an object; it has to be of the form '@enum object ${tree.name.value} { ... }'.")
+          return
+        }
+        var decls = Vector[Stat](
+          q"type Type = Value",
+          q"""sealed trait Value extends scala.Ordered[Value] {
+                def ordinal: $sireumZ
+
+                def name: $sireumString
+
+                final def hash: $sireumZ = hashCode
+
+                final def isEqual(other: Value): $sireumB = this == other
+
+                final def compare(that: Value): $scalaInt = this.ordinal.compareTo(that.ordinal)
+              }
+           """
+          ,
+          q"""final def byName(name: $sireumString): $sireumOption[Value] =
+                elements.elements.find(_.name == name) match {
+                  case _root_.scala.Some(v) => $sireumSomeQ(v)
+                  case _ => $sireumNoneQ()
+              }
+           """,
+          q"""final def byOrdinal(n: $sireumZ): $sireumOption[Value] =
+                if (0 <= n && n < elements.size) $sireumSomeQ(elements(n)) else $sireumNoneQ()
+           """
+        )
+        var elements = Vector[Term.Name]()
+        var i = 0
+        for (stat <- tree.templ.stats) {
+          val sym = stat match {
+            case Lit.Symbol(s) => s.name
+            case Term.Apply(Term.Select(Term.Name("scala"), Term.Name("Symbol")), Seq(Lit.String(s))) => s
+            case _ =>
+              mat.error(stat.pos, s"Slang @enum can only have symbols for enum element names: ${stat.syntax}")
+              return
+          }
+          val tname = Term.Name(sym)
+          val ostats = List[Stat](
+            q"def ordinal: $sireumZ = ${Lit.Int(i)}",
+            q"def name: $sireumString = ${Lit.String(sym)}"
+          )
+          decls :+= q"final case object $tname extends Value { ..$ostats }"
+          elements :+= tname
+          i += 1
+        }
+        decls ++= Vector[Stat](
+          q"val numOfElements: $sireumZ = ${Lit.Int(i)}",
+          q"val elements: $sireumISZ[Value] = $sireumISZQ[Value](..${elements.toList})"
+        )
+        mat.companionMembers.getOrElseUpdate(name, MSeq()) ++= decls.map(_.syntax)
+        mat.companionSupers.getOrElseUpdate(name, MSeq()) += enumSig.syntax
+      case _ =>
+        mat.error(tree.pos, "Slang @enum can only be used on an object.")
+    }
+  }
+}
