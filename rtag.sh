8a444ed26f32782b0045d7e1c6ae565ac433f74d
Robby <robby@k-state.edu>
2019-02-04 11:06:49 -0600

    Updated rtag.sh.

--- rtag.sh
+++ rtag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-VER=$(git log -n 1 --date=format:%Y%m%d --pretty=format:4.%cd.%h)
+VER=$(git log -n 1 --date=format:%Y%m%d.%H%M --pretty=format:4.%cd.%h)
 echo "Tagging ${VER}"
-#git tag ${VER}
-#git push origin ${VER}
+git tag ${VER}
+git push origin ${VER}
